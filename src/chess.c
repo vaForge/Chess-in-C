c766c5c840f4dcb66d6acc010e0110904fbe3763
Varshith <kotipallianjan@gmail.com>
2025-03-02 23:21:36 +0530

    Add check and checkmate functionalities

--- src/chess.c
+++ src/chess.c
@@ -15,6 +15,7 @@ typedef enum {
 typedef struct {
     Type type;
     Color color;
+    int moved;
 }Piece;
 
 typedef struct {
@@ -32,16 +33,17 @@ void init(Chess *chess){
         for(int j=0;j<size;j++){
             chess->piece[i][j].type = noplace;
             chess->piece[i][j].color = noncolor;
+            chess->piece[i][j].moved = 0;
         }
     }
     for(int i=0;i<size;i++){                                     //setting the pawns and colors
         chess->piece[1][i].type = pawn;
-        chess->piece[1][i].color = white;
+        chess->piece[1][i].color = black;     // Fixed: row 1 should be black
         chess->piece[6][i].type = pawn;
-        chess->piece[6][i].color = black;
+        chess->piece[6][i].color = white;     // Fixed: row 6 should be white
     }
 
-    chess->piece[0][0].type = chess->piece[0][7].type = rook;            //Ok black pieces' types  are done  
+    chess->piece[0][0].type = chess->piece[0][7].type = rook;            //black pieces' types
     chess->piece[0][1].type = chess->piece[0][6].type = knight;
     chess->piece[0][2].type = chess->piece[0][5].type = bishop;
     chess->piece[0][3].type = queen;
@@ -57,11 +59,10 @@ void init(Chess *chess){
         chess->piece[0][i].color = black;
         chess->piece[7][i].color = white;
     }
-
 }
 
 void printBoard(Chess *chess){
-    printf("  a  b  c  d  e  f  g  h\n");
+    printf("   a  b  c  d  e  f  g  h\n");
     printf("  -----------------------\n");
     for(int i=0;i<size;i++){
         printf("%d| ",size-i);
@@ -73,18 +74,18 @@ void printBoard(Chess *chess){
                 switch(chess->piece[i][j].type){
                     case pawn:
                         if(chess->piece[i][j].color == white){
-                            printf("p  ");
+                            printf("P  ");
                         }
                         else{
-                            printf("P  ");
+                            printf("p  ");
                         }
                         break;
                     case knight:
                         if(chess->piece[i][j].color == white){
-                            printf("N  ");
+                            printf("H  ");
                         }
                         else{
-                            printf("n  ");
+                            printf("h  ");
                         }
                         break;
                     case bishop:
@@ -125,10 +126,10 @@ void printBoard(Chess *chess){
         printf("\n");
     }
     printf(" ------------------------\n");
-    printf(" a  b  c  d  e  f  g  h\n");
+    printf("   a  b  c  d  e  f  g  h\n");
 }
 
-Move ParseMove( char *str ){
+Move ParseMove(char *str){
     Move move;
     move.fromcol = tolower(str[0]) - 'a';
     move.fromrow = 8 - (str[1] - '0');
@@ -136,66 +137,65 @@ Move ParseMove( char *str ){
     move.torow = 8 - (str[3] - '0');
     return move;
 }
- 
-int makeMove(Chess *chess , Move move){
-    chess->piece[move.torow][move.tocol] = chess->piece[move.fromrow][move.fromcol];
-    chess->piece[move.fromrow][move.fromcol].type = noplace;
-    chess->piece[move.fromrow][move.fromcol].color = noncolor;
-    return 1;
-}
-int validMove(Chess *chess,Move move){
+
+int validMove(Chess *chess, Move move){
     if(move.fromrow<0 || move.fromcol<0 || move.torow< 0 || move.tocol < 0 || move.fromrow>=size || move.fromcol >=size || move.tocol>=size || move.torow>= size){
         return 0;                   //out of board move
     }
     if(chess->piece[move.fromrow][move.fromcol].type == noplace){
-        return 0;                    //moved from emty place 
+        return 0;                    //moved from empty place 
     }
-    if(chess->piece[move.fromrow][move.fromcol].color ==chess->piece[move.torow][move.tocol].color){
+    if(chess->piece[move.fromrow][move.fromcol].color == chess->piece[move.torow][move.tocol].color && 
+       chess->piece[move.torow][move.tocol].type != noplace){
         return 0;                    //move to a place with same color 
     }
     Piece Fromtype = chess->piece[move.fromrow][move.fromcol];
     Piece Totype = chess->piece[move.torow][move.tocol];
     int coldiff = move.fromcol - move.tocol;
     int rowdiff = move.fromrow - move.torow;
-    //Piece restrictions 1)pawn 
+    
+    //Piece restrictions 1) pawn 
     if(Fromtype.type == pawn){
         if(Fromtype.color == white){
-            if(coldiff == 0 && abs(rowdiff)==1 && Totype.type == noplace){
+            if(coldiff == 0 && rowdiff == 1 && Totype.type == noplace){  
                 return 1;
             }
-            if(coldiff ==0 && rowdiff == -2 && move.fromrow == 6 && chess->piece[move.fromrow-1][move.fromcol].type == noplace && Totype.type == noplace){
+            if(coldiff == 0 && rowdiff == 2 && move.fromrow == 6 && 
+               chess->piece[move.fromrow-1][move.fromcol].type == noplace && Totype.type == noplace){
                 return 1;
             }
-            if(abs(coldiff) == 1 && rowdiff == -1 && Totype.type != noplace && Totype.color == black){
+            if(abs(coldiff) == 1 && rowdiff == 1 && Totype.type != noplace && Totype.color == black){
                 return 1;
             }
         }
-        else{
-            if(coldiff == 0 && abs(rowdiff)==1 && Totype.type == noplace){
+        else{                      // black pawn
+            if(coldiff == 0 && rowdiff == -1 && Totype.type == noplace){  
                 return 1;
             }
-            if(coldiff ==0 && rowdiff == 2 && move.fromrow == 1 && chess->piece[move.fromrow+1][move.fromcol].type == noplace && Totype.type == noplace){
+            if(coldiff == 0 && rowdiff == -2 && move.fromrow == 1 && 
+               chess->piece[move.fromrow+1][move.fromcol].type == noplace && Totype.type == noplace){
                 return 1;
             }
-            if(abs(coldiff) == 1 && rowdiff == 1 && Totype.type != noplace && Totype.color == white){
+            if(abs(coldiff) == 1 && rowdiff == -1 && Totype.type != noplace && Totype.color == white){
                 return 1;
             }
         }
         return 0;
     }
+    //2) rook
     if(Fromtype.type == rook){
         if(coldiff == 0 || rowdiff == 0){
             if(rowdiff == 0){
-                int step = coldiff>0?1: -1;
-                for(int i=move.fromcol+step;i!=move.tocol;i+=step){
+                int step = coldiff > 0 ? -1 : 1;  // Fixed: step direction
+                for(int i = move.fromcol + step; i != move.tocol; i += step){
                     if(chess->piece[move.fromrow][i].type != noplace){
                         return 0;
                     }
                 }
             }
             else{
-                int step = rowdiff>0?1: -1;
-                for(int i=move.fromrow+step;i!=move.torow;i+=step){
+                int step = rowdiff > 0 ? -1 : 1;  // Fixed: step direction
+                for(int i = move.fromrow + step; i != move.torow; i += step){
                     if(chess->piece[i][move.fromcol].type != noplace){
                         return 0;
                     }
@@ -205,20 +205,22 @@ int validMove(Chess *chess,Move move){
         }
         return 0;
     }
+    // 3) knight
     if(Fromtype.type == knight){
-        if(abs(coldiff) == 2 && abs(rowdiff) == 1 ){
+        if(abs(coldiff) == 2 && abs(rowdiff) == 1){
             return 1;
         }
-        if(abs(coldiff) == 1 && abs(rowdiff) == 2 ){
+        if(abs(coldiff) == 1 && abs(rowdiff) == 2){
             return 1;
         }
         return 0;
     }
+    //4) bishop
     if(Fromtype.type == bishop){
         if(abs(coldiff) == abs(rowdiff)){
-            int steprow = rowdiff>0?1:-1;
-            int stepcol = coldiff>0?1:-1;
-            for(int i = move.fromrow+steprow,j=move.fromcol+stepcol;i!=move.torow;i+=steprow,j+=stepcol){
+            int steprow = rowdiff > 0 ? -1 : 1;  
+            int stepcol = coldiff > 0 ? -1 : 1;  
+            for(int i = move.fromrow + steprow, j = move.fromcol + stepcol; i != move.torow; i += steprow, j += stepcol){
                 if(chess->piece[i][j].type != noplace){
                     return 0;
                 }
@@ -227,34 +229,35 @@ int validMove(Chess *chess,Move move){
         }
         return 0;
     }
+    //5) king
     if(Fromtype.type == king){
         if(abs(coldiff) <= 1 && abs(rowdiff) <= 1){
             return 1;
         }
         return 0;
     }
-    if(Fromtype.type == queen){                       // rook+bihsop
-        if(coldiff ==0 || rowdiff == 0 || abs(coldiff) == abs(rowdiff)){
-            if(rowdiff == 0){
-                int step = coldiff>0?1: -1;                        //horizontal move
-                for(int i=move.fromcol+step;i!=move.tocol;i+=step){
+    if(Fromtype.type == queen){                                          // rook + bishop = queen
+        if(coldiff == 0 || rowdiff == 0 || abs(coldiff) == abs(rowdiff)){
+            if(rowdiff == 0){                                 // horizontal move
+                int step = coldiff > 0 ? -1 : 1;  
+                for(int i = move.fromcol + step; i != move.tocol; i += step){
                     if(chess->piece[move.fromrow][i].type != noplace){
                         return 0;
                     }
                 }
             }
-            else if(coldiff == 0){
-                int step = rowdiff>0?1: -1;                           // vertical move 
-                for(int i=move.fromrow+step;i!=move.torow;i+=step){
+            else if(coldiff == 0){               // vertical move 
+                int step = rowdiff > 0 ? -1 : 1;  
+                for(int i = move.fromrow + step; i != move.torow; i += step){
                     if(chess->piece[i][move.fromcol].type != noplace){
                         return 0;
                     }
                 }
             }
-            else{                                 //diagonal move
-                int steprow = rowdiff>0?1:-1;
-                int stepcol = coldiff>0?1:-1;
-                for(int i = move.fromrow+steprow,j=move.fromcol+stepcol;i!=move.torow;i+=steprow,j+=stepcol){
+            else{                  // diagonal move
+                int steprow = rowdiff > 0 ? -1 : 1;  
+                int stepcol = coldiff > 0 ? -1 : 1;  
+                for(int i = move.fromrow + steprow, j = move.fromcol + stepcol; i != move.torow; i += steprow, j += stepcol){  
                     if(chess->piece[i][j].type != noplace){
                         return 0;
                     }
@@ -264,22 +267,96 @@ int validMove(Chess *chess,Move move){
         }
         return 0;
     }
+    
+    return 0;  // Added default return value for unexpected piece types
+}
+ 
+int makeMove(Chess *chess, Move move){
+    if(validMove(chess, move) == 0) return 0;
+    chess->piece[move.torow][move.tocol] = chess->piece[move.fromrow][move.fromcol];
+    chess->piece[move.fromrow][move.fromcol].type = noplace;
+    chess->piece[move.fromrow][move.fromcol].color = noncolor;
+    return 1;
+}
+int isKingInCheck(Chess *chess, Color kingColor) {
+    int kingRow = -1, kingCol = -1;
+    for (int r = 0; r < 8; r++) {
+        for (int c = 0; c < 8; c++) {
+            if (chess->piece[r][c].type == king && chess->piece[r][c].color == kingColor) { //king pos found
+                kingRow = r;
+                kingCol = c;
+                break;
+            }
+        }
+    }
+    for (int r = 0; r < 8; r++) {
+        for (int c = 0; c < 8; c++) {
+            if (chess->piece[r][c].color != kingColor && chess->piece[r][c].color != noncolor) {
+                Move testMove = {r, c, kingRow, kingCol};
+                if (validMove(chess, testMove)) {
+                    return 1; // King is in check
+                }
+            }
+        }
+    }
+    return 0; // King is safe
+}
+
+int isCheckmate(Chess *chess, Color kingColor) {
+    if (!isKingInCheck(chess, kingColor)) return 0; 
+    for (int r = 0; r < 8; r++) {
+        for (int c = 0; c < 8; c++) {
+            if (chess->piece[r][c].color == kingColor) { 
+                for (int dr = 0; dr < 8; dr++) {
+                    for (int dc = 0; dc < 8; dc++) {
+                        Move testMove = {r, c, dr, dc};
+                        if (validMove(chess, testMove)) {
+                            // Simulate move
+                            Piece tempFrom = chess->piece[r][c];
+                            Piece tempTo = chess->piece[dr][dc];
+                            chess->piece[dr][dc] = chess->piece[r][c];
+                            chess->piece[r][c].type = noplace;
+                            chess->piece[r][c].color = noncolor;
+                            
+                            int stillInCheck = isKingInCheck(chess, kingColor);
+                            chess->piece[r][c] = tempFrom;        //revet bakc to og move
+                            chess->piece[dr][dc] = tempTo;
+                            if (!stillInCheck) return 0; // Found an escape move
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return 1; // No valid move to escape check
 }
 
 int main(){
     Chess chess;
     init(&chess);
-    printBoard(&chess);
-    char str[5];
-    printf("Enter move (like e2e4): ");
-    scanf("%s",str);
-    while(str[0] != 'Z'){
-        Move move = ParseMove(str);
-        makeMove(&chess,move);
+    Color turn = white;
+    while (1) {
         printBoard(&chess);
-        printf("Enter move (like e2e4): ");
-        scanf("%s",str);
+        char move[5]; 
+        printf("%s's turn: Enter move (e.g., e2e4): ", turn == white ? "White" : "Black");
+        scanf("%s", move);
+    
+        Move m = ParseMove(move);
+        if (chess.piece[m.fromrow][m.fromcol].color == turn && makeMove(&chess, m)) { 
+            if (isKingInCheck(&chess, (turn == white) ? black : white)) {
+                printf("Check!\n");
+                Color opposingColor = (turn == white) ? black : white;
+                if (isCheckmate(&chess, opposingColor)) {
+                    printf("Checkmate! %s wins!\n", turn == white ? "White" : "Black");
+                    exit(0); // End game
+                }
+            }
+            turn = (turn == white) ? black : white; // Switch turn
+        } else {
+            printf("Invalid move! Try again.\n");
+        }
+        
+        
     }
-   
     return 0;
 }
